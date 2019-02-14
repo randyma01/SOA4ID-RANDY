f734d51f331a34d0df4cf352721977752d14c015
Randy <randyma01@gmail.com>
2019-02-14 01:04:52 -0600

    Tarea 1. Ejercicio 2 Completado.

---
+++ tareas/tarea_1/|-- ejercicio2.js
@@ -0,0 +1,34 @@
+//----------------------------------------------------------------------------------------------------//
+// Estudiante: Randy Martinez
+// Carnet: 2014047395
+// Tarea #1: Ejercicios de Introducción a Javascript
+// Ejercicio #2
+//----------------------------------------------------------------------------------------------------//
+
+function verifySide(side){
+    if (typeof side == 'number' && side>0){
+        return true
+    }else{
+        return false
+    }
+}
+
+function findTriangleType (sideA, sideB, sideC){
+    if ((verifySide(sideA) == true) && (verifySide(sideB) == true) && (verifySide(sideC) == true)){
+        if (sideA == sideB && sideB == sideC){
+            alert("The triangle is Equilateral.")
+        } else if (sideA == sideB || sideB == sideC || sideC == sideA){
+            alert("The triangle is Isosceles.")
+        }else{
+            alert("The triangle is Scalene.")
+        }
+    }else{
+        alert("The values of the sides of the triangle are wrong. Check again!")
+    }
+  }
+
+function main(){
+    findTriangleType(2,2,2);
+    findTriangleType(2,3,4);
+    findTriangleType(2,2,5);
+}
