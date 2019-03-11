81211847e2d6bc63e3a263551ec2213445af257e
Randy <randyma01@gmail.com>
2019-03-10 22:27:15 -0600

    Tutorial Finalizado: Mosh

--- tutoriales_personales/react-app/src/App.js
+++ tutoriales_personales/react-app/src/App.js
@@ -1,9 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import logo from './logo.svg';
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 2 },
+      { id: 3, value: 3 },
+      { id: 4, value: 1 },
+      { id: 5, value: 0 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    //console.log(this.state.counters[0]);
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    console.log("Event Handler Called.", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
   render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          //totalCounters={this.state.counters.filter(c => c.cvalue > 0).length}
+          totalCounters={this.state.counters.length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
+
+/* render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -22,7 +78,4 @@ class App extends Component {
         </header>
       </div>
     );
-  }
-}
-
-export default App;
+  } */
--- tutoriales_personales/react-app/src/components/counter.jsx
+++ tutoriales_personales/react-app/src/components/counter.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
+  /* state = {
     value: this.props.counter.value
-  };
+  }; */
 
   /* constructor() {
     super();
     this.handleIncrement = this.handleIncrement.bind(this);
   } */
 
-  handleIncrement = () => {
+  /* handleIncrement = () => {
     console.log("Estoy aumentando en +1.");
     this.setState({ value: this.state.value + 1 });
-  };
+  }; */
 
   render() {
     return (
@@ -21,9 +21,7 @@ class Counter extends Component {
         {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => {
-            this.handleIncrement({ id: 1 });
-          }}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -32,7 +30,6 @@ class Counter extends Component {
           onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
-          {" "}
           Delete
         </button>
       </div>
@@ -41,13 +38,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+    const { value } = this.props.counter;
+    return value === 0 ? <h6>Zero</h6> : value;
   }
 }
 
